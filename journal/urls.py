c49fe7cbc320ce005dbd370db247746de565e91f
Tim Pittman <tim.pittman021@gmail.com>
2019-07-23 00:06:07 -0500

    fixed javascript

--- journal/urls.py
+++ journal/urls.py
@@ -5,8 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.write, name="write"),
     path('note_list/', views.note_list, name="note_list"),
-
-    # path('new/', views.contact_create, name="contact_new"),
+    path('note/new', views.note_create, name="note_new"),
     path('note/<int:id>/edit', views.note_update, name="note_update"),
     # path('<int:id>/', views.show, name="contact_show"),
     # path('delete/<int:id>', views.contact_delete, name='contact_delete'),
--- journal/views.py
+++ journal/views.py
@@ -22,7 +22,17 @@ def note_update(request, id):
          print(request)
          note = Note.objects.all().get(id=id)
          data = json.loads(request.body)
-         note.content = data['content']
+         note.content = data['fields']['content']
          note.save()
          return JsonResponse('ok', safe=False)
+
+@login_required
+def note_create(request):
+    if request.method == "POST":
+
+        data = json.loads(request.body)
+        note = Note(content=data['fields']['content'], word_count=data['fields']['word_count'], pub_date=data['fields']['pub_date'], user=request.user)
+        note.save()
+        return JsonResponse({ 'result': 'OK', 'note': note.content }, status=200)
+        
          
